f61848c4de13852fbfd781c42b2e7e8b5a1d5623
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2024-12-25 19:20:16 +0530

    restructured

--- app/(dashboard)/(routes)/stats/page.tsx
+++ app/(dashboard)/(routes)/stats/page.tsx
@@ -1,7 +1,20 @@
+import Sidebar from "@/components/sidebar";
 import React from "react";
 
-const page = () => {
-  return <div>covid stats</div>;
+const Page = () => {
+  const sidebarItems = [
+    { name: "Chatbot", href: "/chat" },
+    { name: "Stats", href: "/stats" },
+  ];
+
+  return (
+    <div className="flex h-screen">
+      <Sidebar title="ResQ Health" sidebarItems={sidebarItems} />
+      <div className="w-full flex justify-center items-center">
+        Write your stats code here
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ module.exports = {
       },
     },
     extend: {
+      height: {
+        "fill-available": "-webkit-fill-available",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -53,6 +56,16 @@ module.exports = {
           "3": "hsl(var(--chart-3))",
           "4": "hsl(var(--chart-4))",
           "5": "hsl(var(--chart-5))",
+        },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
         },
       },
       borderRadius: {
