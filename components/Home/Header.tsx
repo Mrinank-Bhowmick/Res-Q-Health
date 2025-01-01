aa7e689e6dcca60901d0d7ae9e43461a7db08162
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2025-01-01 14:29:56 +0530

    Update Header.tsx

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 const menuItems = [
   { name: "About", href: "#" },
@@ -45,14 +46,9 @@ const Header = () => {
           </ul>
         </div>
         <div className="hidden lg:block">
-          <a
-            href="#"
-            className="group flex h-10 items-center justify-center rounded-md border border-orange-600 bg-gradient-to-b from-orange-400 via-orange-500 to-orange-600 px-4 text-neutral-50 shadow-[inset_0_1px_0px_0px_#fdba74] active:[box-shadow:none]"
-          >
-            <span className="block group-active:[transform:translate3d(0,1px,0)]">
-              Login
-            </span>
-          </a>
+          <button className="group flex h-10 items-center justify-center rounded-md border border-orange-600 bg-gradient-to-b from-orange-400 via-orange-500 to-orange-600 px-4 text-neutral-50 shadow-[inset_0_1px_0px_0px_#fdba74] active:[box-shadow:none]">
+            <Link href="/sign-in">Login</Link>
+          </button>
         </div>
         <div className="lg:hidden">
           <Menu onClick={toggleMenu} className="size-6 cursor-pointer" />
